0365dcefb0e0c0548e9df901ce6e6825a2e987f9
Gopinath Achuthan <agopinathus@gmail.com>
2021-04-27 15:18:55 -0400

    problem 146

---
+++ 146. LRU Cache.py
@@ -0,0 +1,28 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.cache.keys():
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache.keys():
+            self.cache[key] = value
+            self.cache.move_to_end(key)
+        else:
+            self.cache[key] = value
+            
+        while len(self.cache)>self.capacity:
+            self.cache.popitem(last=False)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
