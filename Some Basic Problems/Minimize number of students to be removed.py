890f9b04929685e471785904526a659806cab8d6
Gopinath Achuthan <agopinathus@gmail.com>
2021-05-30 18:23:24 -0400

    MS interview geeksforgeeks

--- Some Basic Problems/Minimize number of students to be removed.py
+++ Some Basic Problems/Minimize number of students to be removed.py
@@ -1,20 +1,62 @@
-# Minimize number of students to be removed
+'''
+**Minimize number of Students to be removed**
+
+N Students of different heights are attending an assembly. 
+The heights of th students are represented by an array H[].
+The problem is that if a student has less or equal height 
+than the students standing in front of him, then he/she 
+cannot see the assembly. Find the minimum number of students 
+to be removed such that maximum possible numbers of students
+can see the assembly.
+
+Expected Time Complexity: O(N log N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1<=N<=10^5
+1<=H[i]<=10^5
+
+'''
+
 
 def removeStudents(H, N):
     # code here
-    L = [0 for i in range(N)]
+    L = [H[0]]
+    
+    for curr in H[1:]:
+        if L[-1]<curr:
+            L.append(curr)
+        else:
+            index = findSmallestBigNumberIndex(curr,L)
+            print(index)
+            L[index] = curr
 
-    for i in range(N):
-        for j in range(i):
-            if H[j]<=H[i] and L[j]>L[i]:
-                L[i] = L[j]
-        L[i]+=1
+    return N-len(L)
 
-    return N-max(L)
 
+def findSmallestBigNumberIndex(key, arr):
+    left = 0
+    right = len(arr)
+
+    while(left<=right):
+        mid = (right+left)//2
+        if arr[mid] == key:
+            return mid
+        elif arr[mid]<key:
+            if mid+1<len(arr) and arr[mid+1]>key:
+                return mid+1
+            else:
+                left = mid+1
+        else:
+            if mid-1>0 and arr[mid-1]<key:
+                return mid
+            else:
+                right = mid-1
+    return mid
 
 if __name__ == '__main__':
  
-    A = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+    A = [1, 8, 4, 4, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+    
     print(len(A), A)
     print(removeStudents(A, len(A)))
