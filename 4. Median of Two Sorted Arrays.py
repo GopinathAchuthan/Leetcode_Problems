f12bd0a8661172bf9c6ec9a25c1589a1b389e821
Gopinath Achuthan <agopinathus@gmail.com>
2021-06-06 17:48:23 -0400

    prob 4

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -1,3 +1,34 @@
+# Time Complexity = O(log(m+n))
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        # code here
+        half = (len(nums1)+len(nums2))//2
+        
+        if len(nums1)>len(nums2):
+            nums1,nums2 = nums2,nums1
+        
+        l = 0
+        r = len(nums1)-1
+        while(True):
+            i = l+(r-l)//2      # nums1 mid pointer
+            j = half-i-2        # nums2 mid pointer
+            
+            nums1Left = nums1[i] if i>=0 else float('-inf')
+            nums1Right = nums1[i+1] if i+1<len(nums1) else float('inf')
+            nums2Left = nums2[j] if j>=0 else float('-inf')
+            nums2Right = nums2[j+1] if j+1<len(nums2) else float('inf')
+            
+            if nums1Left<=nums2Right and nums2Left<=nums1Right:
+                if (len(nums1)+len(nums2))%2:
+                    return min(nums1Right,nums2Right)
+                else:
+                    return (max(nums1Left,nums2Left)+min(nums1Right,nums2Right))/2
+            elif nums1Left>nums2Right:
+                r = i-1
+            else:
+                l = i+1
+
+# Time Complexity = O(m+n)
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         lenNums1 = len(nums1)
