b952883846ed9e73306f98668ebe022dd933cf59
Gopinath Achuthan <agopinathus@gmail.com>
2021-07-03 22:05:25 -0400

    Minimum Lights to Activate

--- InterviewBit Problems/Arrays/Minimum Lights to Activate.py
+++ InterviewBit Problems/Arrays/Minimum Lights to Activate.py
@@ -4,13 +4,13 @@ class Solution:
     # @return an integer
     def solve(self, A, B):
         n = len(A)
-        left, right = 0, min(B-1, n-1)
+        left, right = -1, min(B-1, n-1)
         pos, count = -1, 0
         last = 0
 
         while(last<n):
             pos = -1
-            for i in range(right,left-1,-1):
+            for i in range(right,left,-1):
                 if A[i] == 1:
                     pos = i
                     break
@@ -20,6 +20,6 @@ class Solution:
             
             count +=1
             last = pos+B
-            left, right = pos+1, min(n-1, pos+2*B-1)
+            left, right = pos, min(n-1, pos+2*B-1)
         
         return count
