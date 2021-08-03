c35aad4ea40df4792004b92d70e814a4d53915cb
Gopinath Achuthan <agopinathus@gmail.com>
2021-08-02 21:54:15 -0400

    update prob 543

--- 543. Diameter of Binary Tree.py
+++ 543. Diameter of Binary Tree.py
@@ -6,18 +6,23 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        def dfs(node):
-            if not node:
+        def post_order_traversal(node):
+            # base case:
+            if not node.left and not node.right:
                 return 0
+            l_pathMax = 0
+            r_pathMax = 0
+            if node.left:
+                l_pathMax = post_order_traversal(node.left)+1
+            if node.right:
+                r_pathMax = post_order_traversal(node.right)+1
+                
+            if post_order_traversal.diameter< l_pathMax+r_pathMax:
+                post_order_traversal.diameter = l_pathMax+r_pathMax
+            return max(l_pathMax, r_pathMax)
             
-            left = dfs(node.left)
-            right = dfs(node.right)
+        post_order_traversal.diameter = 0
+        post_order_traversal(root)
+        return post_order_traversal.diameter
             
-            dfs.diameter = max(dfs.diameter, left+right+1)
-            return max(left,right)+1
-        
-        
-        dfs.diameter = 0
-        dfs(root)
-        
-        return dfs.diameter-1
+            
