b07806355ef192986efa6d7480867f522532d594
Gopinath Achuthan <agopinathus@gmail.com>
2021-08-25 13:43:23 -0400

    prob 90

--- 90. Subsets II.py
+++ 90. Subsets II.py
@@ -1,3 +1,28 @@
+# Method 1
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        self.__helper(nums, [], 0, res)
+        return res
+    
+    def __helper(self, arr, slate, i, res):
+        if i == len(arr):
+            res.append(list(slate))
+        else:
+            # exclude
+            k = i+1
+            while(k<len(arr) and arr[i]==arr[k]):
+                k+=1
+            self.__helper(arr, slate, k, res)
+            
+            # include
+            slate.append(arr[i])
+            self.__helper(arr, slate, i+1, res)
+            slate.pop()
+        return
+
+# Method 2
 class Solution:
     def generateSubsets(self, subsets, curr_subset, index, nums):
         subsets.append(curr_subset)
