baf3d72184ab4077672e6a2f5726535c77ddcfdf
Gopinath Achuthan <agopinathus@gmail.com>
2021-08-25 15:16:25 -0400

    prob 238

--- 238. Product of Array Except Self.py
+++ 238. Product of Array Except Self.py
@@ -1,3 +1,25 @@
+# Method 1:
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        ans = [0]*length
+        ans[0] = 1
+        
+        for i in range(1,length):
+            ans[i] = nums[i-1]*ans[i-1]
+        
+        right = 1
+        for i in range(length-1,-1,-1):
+            ans[i] = ans[i]*right
+            right*=nums[i]
+        
+        return ans
+        
+        
+        
+# Method 2: By keeping track of number of zeros in the nums array
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         numOfZeros = 0
