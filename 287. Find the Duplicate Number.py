9b4167a54276fa2f75a15c097a15c8561331b55a
Gopinath Achuthan <agopinathus@gmail.com>
2021-08-25 15:16:43 -0400

    prob 287

--- 287. Find the Duplicate Number.py
+++ 287. Find the Duplicate Number.py
@@ -1,3 +1,23 @@
+# Method 1: Floyd's Tortoise and Hare
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = nums[0], nums[0]
+        while True:
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+            if fast == slow:
+                break
+        
+        slow = nums[0]
+        while fast!=slow:
+            slow, fast = nums[slow], nums[fast]
+        return slow
+
+# Method 2: Set
+# Time Complexity: O(N)
+# Space Complexity: O(N)
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         mem = set()
