e7f41fc7ce45bf63a85008c006ca6bbb79c3d37a
Gopinath Achuthan <agopinathus@gmail.com>
2021-12-20 11:18:31 -0500

    update 268

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,4 +1,26 @@
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
+# Method 1: Mathematics answer
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         return (n*(n+1)//2) - sum(nums)
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+
+# Method 2: Cycle Sort
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        
+        for i in range(n):
+            while(i!=nums[i] and nums[i]!=n):
+                nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
+        
+        for i in range(n):
+            if nums[i]!=i:
+                return i
+            
+        return n
