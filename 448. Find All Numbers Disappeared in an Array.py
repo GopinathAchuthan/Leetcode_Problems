b55d07ebc47e583e437d64a16d60468e09e84846
Gopinath Achuthan <agopinathus@gmail.com>
2021-12-20 12:22:13 -0500

    update 448

--- 448. Find All Numbers Disappeared in an Array.py
+++ 448. Find All Numbers Disappeared in an Array.py
@@ -1,5 +1,27 @@
+# Time Complexity: O(N)
+# Space Complexity: O(N)
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         n = len(nums)
         mem = set([i for i in range(1,n+1)])
         return list(mem-set(nums))
+
+
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        res = []
+        for i in range(n):
+            while(i+1 != nums[i]):
+                d = nums[i]
+                if nums[d-1]==d:
+                    break
+                nums[d-1] , nums[i] = nums[i], nums[d-1]
+        
+        for i in range(n):
+            if i+1!=nums[i]:
+                res.append(i+1)
+        
+        return res
