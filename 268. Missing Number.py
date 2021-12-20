34838746a77051a8e657a327fec8f49b090ffa1c
Gopinath Achuthan <agopinathus@gmail.com>
2021-12-20 11:23:09 -0500

    update 268

--- 268. Missing Number.py
+++ 268. Missing Number.py
@@ -1,15 +1,15 @@
 # Time Complexity: O(N)
 # Space Complexity: O(1)
-
 # Method 1: Mathematics answer
+# In this method there is a possibility of integer overflow.
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         return (n*(n+1)//2) - sum(nums)
 
+
 # Time Complexity: O(N)
 # Space Complexity: O(1)
-
 # Method 2: Cycle Sort
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
